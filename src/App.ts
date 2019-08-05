b9b7dc8dbea8ccf18ab7d5480c7f5fd1f77a7534
Alan <alan.silva.n@hotmail.com>
2019-08-05 19:25:51 -0300

    Setting middlewares at the controller

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 import routes from './routes'
 import mongoose from 'mongoose'
+import morgan from 'morgan'
+import cors from 'cors'
+import path from 'path'
 import * as dotenv from 'dotenv'
 
 class App {
@@ -9,9 +12,21 @@ class App {
     public constructor () {
         this.express = express()
 
+        this.middlewares()
         this.database()
         this.routes()
         this.dotEnv()
+    }
+
+    private middlewares (): void {
+        this.express.use(express.json())
+        this.express.use(express.urlencoded({ extended: true }))
+        this.express.use(morgan('dev'))
+        this.express.use(cors())
+        this.express.use(
+            '/files',
+            express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+        )
     }
 
     private database (): void {
