12eaaaec4c0fc0af45b29daaa69e7c46504bf486
Alan <alan.silva.n@hotmail.com>
2019-08-05 19:09:57 -0300

    GET and DELETE routes

--- src/controllers/UploadController.ts
+++ src/controllers/UploadController.ts
@@ -7,6 +7,17 @@ class UploadController {
         const post = await Post.create({ name, size, key, url })
         return res.json(post)
     }
+
+    public async show (req: Request, res: Response): Promise<Response> {
+        const post = await Post.find()
+        return res.json(post)
+    }
+
+    public async delete (req: Request, res: Response): Promise<Response> {
+        const post = await Post.findById(req.params.id)
+        await post.remove()
+        return res.send('Deleted')
+    }
 }
 
 export default new UploadController()
--- src/routes.ts
+++ src/routes.ts
@@ -2,15 +2,11 @@ import { Router } from 'express'
 import multer from 'multer'
 import MulterConfig from './config/MulterConfig'
 import UploadController from './controllers/UploadController'
-import multerS3 from 'multer-s3'
-import aws from 'aws-sdk'
 
 const routes = Router()
 
-const storageS3 = new aws.S3({
-    accessKeyId: ''
-})
-
+routes.get('/upload', UploadController.show)
 routes.post('/upload', multer(MulterConfig).single('file'), UploadController.create)
+routes.delete('/upload/:id', UploadController.delete)
 
 export default routes
